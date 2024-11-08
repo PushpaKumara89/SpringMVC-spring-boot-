09e8aef1f56307ea1a6f23a9956f954e20aa33cb
kpush <kpushpakumarajd@gmail.com>
2024-11-09 00:21:08 +0530

    Model and ModelMap

--- src/main/java/com/appk/springmvc/HomeController.java
+++ src/main/java/com/appk/springmvc/HomeController.java
@@ -1,12 +1,10 @@
 package com.appk.springmvc;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
@@ -14,14 +12,19 @@ public class HomeController {
     public String home(){
         return "index";
     }
-    @RequestMapping("add")
-    public ModelAndView add(@RequestParam("num1") int i, @RequestParam("num2") int j){
+
+   /* @RequestMapping("add")
+    public String add(@RequestParam("num1") int i, @RequestParam("num2") int j, ModelMap m){
         int sum = i+j;
-
-        ModelAndView mv = new ModelAndView();
-        mv.setViewName("result");
-        mv.addObject("sum", sum);
-
-        return mv;
+        m.addAttribute("sum", sum);
+        return "result";
+    }*/
+    @RequestMapping("add")
+    public String add(@RequestParam("num1") int i, @RequestParam("num2") int j, Model m){
+        int sum = i+j;
+        m.addAttribute("sum", sum);
+        return "result";
     }
 }
+
+// Model and ModelMap
